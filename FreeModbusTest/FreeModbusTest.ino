d06a0f14feadd38023e9f2d4c50f0bee8f5370f7
Marek Czerski <m.czerski@ap-tech.pl>
2024-09-14 00:32:43 +0200

    implementation of simple dimmer with scene controller

--- FreeModbusTest/FreeModbusTest.ino
+++ FreeModbusTest/FreeModbusTest.ino
@@ -1,41 +1,70 @@
 #include "mb_with_wdt.h"
+#include <Dimmer.h>
+#include <BounceSwitch.h>
+#include <SceneController.h>
 
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 8
-#define MOTION_PIN 3
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 2
 
-uint16_t holding[10] = {0};
+using namespace mys_toolkit;
+
+BounceSwitch sw1(3, Duration(50), true);
+BounceSwitch sw2(4, Duration(50), true);
+CwWwDimmer dimmer(5, 6, false, 10, {.slowDimming=1, .fullBrightness=1});
+SceneController scene;
 
 void setup() {
-  pinMode(MOTION_PIN, INPUT_PULLUP);
   eMBInitWithWDT(MB_RTU, 0, WDTO_8S, VERSION_MAJOR, VERSION_MINOR);
-  holding[1] = (VERSION_MAJOR << 8) + VERSION_MINOR;
+  dimmer.begin();
 }
 
 void loop() {
-  holding[2] = digitalRead(MOTION_PIN);
+  dimmer.update(sw1.update());
+  scene.update(sw2.update());
   eMBPollWithWDT();
 }
 
 eMBErrorCode eMBRegInputCB(UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs) {
+  if (usAddress >= 0 or usAddress + usNRegs - 1 <= 2) {
+    for (USHORT i = 0; i < usNRegs; i++) {
+      switch (i) {
+        case 0:
+           pucRegBuffer[i * 2 + 0] = 0;
+           pucRegBuffer[i * 2 + 1] = dimmer.getLevel();
+           break;
+        case 1:
+           pucRegBuffer[i * 2 + 0] = 0;
+           pucRegBuffer[i * 2 + 1] = scene.getScene0Counter();
+           break;
+        case 2:
+           pucRegBuffer[i * 2 + 0] = 0;
+           pucRegBuffer[i * 2 + 1] = scene.getScene1Counter();
+           break;
+        default:
+           break;
+      }
+    }
+    return MB_ENOERR;
+  }
   return MB_ENOREG;
 }
 
 eMBErrorCode eMBRegHoldingCB2(UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode) {
-  if (usAddress + (usNRegs - 1) >= (sizeof(holding) / sizeof(holding[0]))) return MB_ENOREG;
-
-  for (uint16_t i = 0; i < usNRegs; i++) {
-    if (eMode == MB_REG_READ) {
-      pucRegBuffer[i * 2 + 0] = (unsigned char) (holding[i + usAddress] >> 8);
-      pucRegBuffer[i * 2 + 1] = (unsigned char) (holding[i + usAddress] & 0xFF);
-    } else if (eMode == MB_REG_WRITE) {
-      if (usAddress == 0) {
-        return MB_ENOREG;
+  if (usAddress >= 0 or usAddress + usNRegs - 1 <= 0) {
+    for (USHORT i = 0; i < usNRegs; i++) {
+      if (eMode == MB_REG_WRITE) {
+        switch (i) {
+          case 0:
+            dimmer.request(pucRegBuffer[i * 2 + 1]);
+            break;
+          default:
+            break;            
+        }
+        return MB_ENOERR;
       }
-      holding[i + usAddress] = (pucRegBuffer[i * 2 + 0] << 8) | pucRegBuffer[i * 2 + 1];
     }
   }
-  return MB_ENOERR;
+  return MB_ENOREG;
 }
 
 eMBErrorCode eMBRegCoilsCB(UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNCoils, eMBRegisterMode eMode) {
