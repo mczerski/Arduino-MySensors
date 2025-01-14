bfece090f2d0f88a6a697df5187503410bf3cb43
Marek Czerski <m.czerski@ap-tech.pl>
2025-01-14 14:08:07 +0100

    remove motion sensor from dimmer + update to newest FreemodbusAVR version

--- ModbusDimmer/ModbusDimmer.ino
+++ ModbusDimmer/ModbusDimmer.ino
@@ -4,7 +4,7 @@
 #include <SceneController.h>
 
 #define VERSION_MAJOR "2"
-#define VERSION_MINOR "10"
+#define VERSION_MINOR "11"
 
 #define KITCHEN_LED
 
@@ -65,15 +65,6 @@ SimpleDimmer dimmer1(5, true, 10, {.slowDimming=1, .fullBrightness=1});
 SceneController scene2;
 #endif
 
-#ifdef KITCHEN_MOTION
-#define SLAVE_ID 4
-#define SLAVE_NAME "Kitchen Motion"
-#define MOTION
-#define MOTION_PIN 3
-#define FLASH_ID 0x1F65
-#define LED_PIN A1
-#endif
-
 #ifdef KITCHEN_LED
 #include <APDS9930Switch.h>
 #include <MiLightDimmer.h>
@@ -102,156 +93,106 @@ SceneController scene3;
 
 const char additional_info[] = SLAVE_NAME " v" VERSION_MAJOR "." VERSION_MINOR;
 SPIFlash flash(8, FLASH_ID);
+struct InputRegisters_ {
+  uint16_t dimmer1_state;
+  uint16_t dimmer2_state;
+  uint16_t scene1_0_counter;
+  uint16_t scene1_1_counter;
+  uint16_t scene2_0_counter;
+  uint16_t scene2_1_counter;
+  uint16_t scene3_0_counter;
+  uint16_t scene3_1_counter;
+  uint16_t scene4_0_counter;
+  uint16_t scene4_1_counter;
+  uint16_t apds0030_status;
+  uint16_t milight_status;
+};
+typedef ModbusDataBlock<InputRegisters_> InputRegisters;
+InputRegisters input_register = {0};
 
 void setup() {
   eMBInitWithWDT(MB_RTU, 0, WDTO_8S, SLAVE_ID, additional_info, sizeof(additional_info) - 1, &flash, LED_PIN);
-  #ifdef DIMMER1
+#ifdef DIMMER1
   dimmer1.begin();
-  #endif
-  #ifdef DIMMER2
+#endif
+#ifdef DIMMER2
   dimmer2.begin();
-  #endif
-  #ifdef MOTION
-  pinMode(MOTION_PIN, INPUT_PULLUP);
-  #endif
-  #ifdef APDS9930
+#endif
+#ifdef APDS9930
   apds9930.init();
-  #endif
+#endif
   Duration::setPrescaler(PRESCALER);
 }
 
 void loop() {
-  #ifdef DIMMER1
+#ifdef DIMMER1
   dimmer1.update(sw1.update());
-  #endif
-  #ifdef DIMMER2
+  input_register.s.dimmer1_state = dimmer1.getPercent();
+#endif
+#ifdef DIMMER2
   dimmer2.update(sw2.update());
-  #endif
-  #ifdef SCENE1
+  input_register.s.dimmer2_state = dimmer2.getPercent();
+#endif
+#ifdef SCENE1
   scene1.update(sw1.update());
-  #endif
-  #ifdef SCENE2
+  input_register.s.scene1_0_counter = scene1.getScene0Counter();
+  input_register.s.scene1_1_counter = scene1.getScene1Counter();
+#endif
+#ifdef SCENE2
   scene2.update(sw2.update());
-  #endif
-  #ifdef SCENE3
+  input_register.s.scene2_0_counter = scene2.getScene0Counter();
+  input_register.s.scene2_1_counter = scene2.getScene1Counter();
+#endif
+#ifdef SCENE3
   scene3.update(sw3.update());
-  #endif
-  #ifdef SCENE4
+  input_register.s.scene3_0_counter = scene3.getScene0Counter();
+  input_register.s.scene3_1_counter = scene3.getScene1Counter();
+#endif
+#ifdef SCENE4
   scene4.update(sw4.update());
-  #endif
-  #ifdef APDS9930
+  input_register.s.scene4_0_counter = scene4.getScene0Counter();
+  input_register.s.scene4_1_counter = scene4.getScene1Counter();
+#endif
+#ifdef APDS9930
   apds9930.update();
-  #endif
+  input_register.s.apds0030_status = (apds9930.getInt(0) << 8) + apds9930.getErrorCode(0);
+#endif
+#ifdef MI_LIGHT
+  input_register.s.milight_status = dimmer2.getErrorCode();
+#endif
   eMBPollWithWDT();
 }
 
 eMBErrorCode eMBRegInputCB(UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs) {
-  if (usAddress >= 1 and usAddress + usNRegs - 1 <= 12) {
-    for (USHORT i = 0; i < usNRegs; i++) {
-      switch (usAddress + i) {
-#ifdef DIMMER1
-        case 1:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = dimmer1.getPercent();
-           break;
-#endif
-#ifdef DIMMER2
-        case 2:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = dimmer2.getPercent();
-           break;
-#endif
-#ifdef SCENE1
-        case 3:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene1.getScene0Counter();
-           break;
-        case 4:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene1.getScene1Counter();
-           break;
-#endif
-#ifdef SCENE2
-        case 5:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene2.getScene0Counter();
-           break;
-        case 6:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene2.getScene1Counter();
-           break;
-#endif
-#ifdef SCENE3
-        case 7:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene3.getScene0Counter();
-           break;
-        case 8:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene3.getScene1Counter();
-           break;
-#endif
-#ifdef SCENE4
-        case 9:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene4.getScene0Counter();
-           break;
-        case 10:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = scene4.getScene1Counter();
-           break;
-#endif
-#ifdef MOTION
-        case 11:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = digitalRead(MOTION_PIN);
-           break;
-#endif
-#ifdef APDS9930
-        case 12:
-           pucRegBuffer[i * 2 + 0] = apds9930.getInt(0);
-           pucRegBuffer[i * 2 + 1] = apds9930.getErrorCode(0);
-           break;
-#endif
-#ifdef MI_LIGHT
-        case 13:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = dimmer2.getErrorCode();
-           break;
-#endif
-        default:
-           pucRegBuffer[i * 2 + 0] = 0;
-           pucRegBuffer[i * 2 + 1] = 0;
-           break;
-      }
-    }
-    return MB_ENOERR;
-  }
-  return MB_ENOREG;
+  return eMBCopyToRegBuffer(input_register, pucRegBuffer, usAddress, usNRegs);
 }
 
-eMBErrorCode eMBRegHoldingCB2(UCHAR * pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode) {
+eMBErrorCode eMBRegHoldingCB(USHORT * pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode) {
+  uint8_t dimmer1_state = 0;
+  uint8_t dimmer2_state = 0;
   if (usAddress >= 1 and usAddress + usNRegs - 1 <= 2) {
     for (USHORT i = 0; i < usNRegs; i++) {
       if (eMode == MB_REG_WRITE) {
         switch (usAddress + i) {
 #ifdef DIMMER1
           case 1:
-            if (pucRegBuffer[i * 2 + 1] == 255) {
+            dimmer1_state = __builtin_bswap16(pucRegBuffer[i]);
+            if (dimmer1_state == 255) {
               dimmer1.set(true);
             }
             else {
-              dimmer1.setPercent(pucRegBuffer[i * 2 + 1]);
+              dimmer1.setPercent(dimmer1_state);
             }
             break;
 #endif
 #ifdef DIMMER2
           case 2:
-            if (pucRegBuffer[i * 2 + 1] == 255) {
+            dimmer2_state = __builtin_bswap16(pucRegBuffer[i]);
+            if (dimmer2_state == 255) {
               dimmer2.set(true);
             }
             else {
-              dimmer2.setPercent(pucRegBuffer[i * 2 + 1]);
+              dimmer2.setPercent(dimmer2_state);
             }
             break;
 #endif
