9d2fa93d441a96aa192136b190765e4f2c3fcefb
Marek Czerski <m.czerski@ap-tech.pl>
2025-01-14 14:41:53 +0100

    add measurement tresholds for sensor + add pressure measurement

--- ModbusSensor/ModbusSensor.ino
+++ ModbusSensor/ModbusSensor.ino
@@ -3,9 +3,9 @@
 #include <ArtronShop_BH1750.h>
 
 #define VERSION_MAJOR "1"
-#define VERSION_MINOR "1"
+#define VERSION_MINOR "2"
 
-#define TEST
+#define KITCHEN_MOTION
 
 #ifdef TEST
 #define SLAVE_ID 0
@@ -42,9 +42,9 @@ BME280I2C bmeSensor(
     BME280::OSR_X1,
     BME280::OSR_X1,
     BME280::OSR_X1,
-    BME280::Mode_Forced,
+    BME280::Mode_Normal,
     BME280::StandbyTime_1000ms,
-    BME280::Filter_Off,
+    BME280::Filter_16,
     BME280::SpiEnable_False,
     0x77
   )
@@ -57,9 +57,10 @@ struct InputRegisters_ {
   uint16_t luminance;
   uint16_t humidity;
   uint16_t temperature;
+  uint16_t pressure;
 };
 typedef ModbusDataBlock<InputRegisters_> InputRegisters;
-InputRegisters input_register;
+InputRegisters input_register = {0};
 
 
 void setup() {
@@ -83,11 +84,16 @@ void loop() {
   input_register.s.motion_state = digitalRead(MOTION_PIN);
 #endif
 #ifdef BH1750_SENSOR
-  input_register.s.luminance = round(bh1750.light());
+  uint16_t luminance = round(bh1750.light());
+  if (abs(luminance - input_register.s.luminance) > 3) input_register.s.luminance = luminance;
 #endif
 #ifdef BME280_SENSOR
-  input_register.s.humidity = round(bmeSensor.hum());
-  input_register.s.temperature = round(bmeSensor.temp() * 10);
+  uint16_t humidity = round(bmeSensor.hum());
+  uint16_t temperature = round(bmeSensor.temp() * 10);
+  uint16_t pressure = round(bmeSensor.pres() / 10);
+  if (abs(humidity - input_register.s.humidity) > 3) input_register.s.humidity = humidity;
+  if (abs(temperature - input_register.s.temperature) > 5) input_register.s.temperature = temperature;
+  if (abs(pressure - input_register.s.pressure) > 5) input_register.s.pressure = pressure;
 #endif
 }
 
